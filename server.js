2e197c027b23fa2aa697842cad1c97467a016880
hossein <namazian87@gmail.com>
2018-08-14 08:56:44 +0430

    initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const fs = require('fs');
+const hbs =require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var log = `${new Date().toString()}, ${req.method}, ${req.url} \n`;
+    fs.appendFile('server.log', log, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    })
+    console.log(log);
+    next();
+})
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+})
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to web page',
+    });
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page!',
+    });
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMsg: 'Bad Request!'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
